759aad26fadb18759d0f05a8d84c059fb9c14574
Louis Ouellet <louis_ouellet@hotmail.com>
2022-12-15 18:31:49 -0500

    2022-12-15 18:31 - v1.0.4

--- src/BaseCommand.php
+++ src/BaseCommand.php
@@ -38,6 +38,45 @@ class BaseCommand {
     return false;
   }
 
+  protected function call($method, $url, $data = false){
+    $curl = curl_init();
+    switch ($method){
+      case"POST":
+        curl_setopt($curl, CURLOPT_POST, 1);
+        if($data){
+          curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        }
+        break;
+      case"PUT":
+        curl_setopt($curl, CURLOPT_PUT, 1);
+        break;
+      default:
+        if($data){
+          $url = sprintf("%s?%s", $url, http_build_query($data));
+        }
+    }
+    // Optional Authentication:
+    $auth = $url;
+    if(str_contains($auth, '@')){
+      $auth = explode('@',$auth)[0];
+      if(str_contains($auth, '//')){ $auth = explode('//',$auth)[1]; }
+      if(str_contains($auth, ':')){
+        curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        curl_setopt($curl, CURLOPT_USERPWD, $auth);
+      } else {
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json' , "Authorization: Bearer $auth"));
+      }
+      $url = str_replace($auth.'@','',$url);
+    }
+
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+    $result = curl_exec($curl);
+    curl_close($curl);
+    return json_decode($result,true);
+  }
+
   protected function output($string) {
     print_r($string . PHP_EOL);
   }
